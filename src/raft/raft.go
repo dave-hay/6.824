0bf16c13c09b97223d641f7a8e1efa79ef322b0c
dave-hay <davehay93@gmail.com>
2024-03-23 18:58:53 -0700

    Start()

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -242,24 +242,21 @@ func (rf *Raft) startElection() {
 
 }
 
-// the service using Raft (e.g. a k/v server) wants to start
-// agreement on the next command to be appended to Raft's log. if this
-// server isn't the leader, returns false. otherwise start the
-// agreement and return immediately. there is no guarantee that this
-// command will ever be committed to the Raft log, since the leader
-// may fail or lose an election. even if the Raft instance has been killed,
-// this function should return gracefully.
-//
-// the first return value is the index that the command will appear at
-// if it's ever committed. the second return value is the current
-// term. the third return value is true if this server believes it is
-// the leader.
+// Start agreement to append command to replicated log
 func (rf *Raft) Start(command interface{}) (int, int, bool) {
 	index := -1
 	term := -1
-	isLeader := true
+	isLeader := false
 
-	// Your code here (2B).
+	// if not leader return
+	rf.mu.Lock()
+	if rf.state == Leader {
+		index = rf.commitIndex
+		term = rf.currentTerm
+		isLeader = true
+		return index, term, isLeader
+	}
+	rf.mu.Unlock()
 
 	return index, term, isLeader
 }
