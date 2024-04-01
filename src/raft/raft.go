1e648cba4875a0f58c1ca3923fa757aecd911535
dave-hay <davehay93@gmail.com>
2024-04-01 13:45:08 -0700

    Request vote

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -104,16 +104,30 @@ type RequestVoteReply struct {
 	voteGranted bool
 }
 
-// example RequestVote RPC handler.
+// receiver of request vote
 func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
 	// if candidates term < voters term; candidate becomes follower
 	if args.CandidateTerm < rf.currentTerm {
 		reply.Term = rf.currentTerm
 		reply.voteGranted = false
-		return 
+		return
 	}
+
+	isVoterValid := rf.votedFor == -1 || rf.votedFor == args.CandidateId
+	// candidates log is at least as up to date as voters log
+	isCandidateValid := args.CandidateLastLogIndex >= len(rf.logs)-1
+
+	if isVoterValid && isCandidateValid {
+		rf.currentTerm = reply.Term
+		reply.voteGranted = true
+	}
+
 }
 
+// initiated by candidates during election
 func (rf *Raft) sendRequestVote(server int, args *RequestVoteArgs, reply *RequestVoteReply) bool {
 	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
 	return ok
