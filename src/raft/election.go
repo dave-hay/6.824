e547325df87e93257279c06f5d99265369409bfe
dave-hay <davehay93@gmail.com>
2024-04-02 15:46:04 -0700

    basic election, need err handling

--- src/raft/election.go
+++ src/raft/election.go
@@ -44,16 +44,40 @@ func (rf *Raft) RequestVote(args *RequestVoteArgs, reply *RequestVoteReply) {
 
 // sendRequestVote method
 // called by candidates during election to request a vote from a Raft instance
-func (rf *Raft) sendRequestVote(server int, es *ElectionState, args *RequestVoteArgs) bool {
-	reply := RequestVoteReply{}
-	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
-	return ok
-}
+func (rf *Raft) sendRequestVote(server int, voteChannel chan int, errChannel chan int) {
 
-type ElectionState struct {
-	voteCount         int
-	convertToFollower bool
-	newTerm           int
+	rf.mu.Lock()
+	args := &RequestVoteArgs{
+		CandidateId:           rf.me,
+		CandidateTerm:         rf.currentTerm,
+		CandidateLastLogIndex: len(rf.logs) - 1,
+		CandidateLastLogTerm:  rf.logs[len(rf.logs)-1].Term,
+	}
+	reply := &RequestVoteReply{}
+	rf.mu.Unlock()
+
+	// keep calling while error
+	// keep calling until timeout
+	ok := rf.peers[server].Call("Raft.RequestVote", args, reply)
+
+	if !ok {
+		//  !ok means that there was an error and should re-send the request vote
+		errChannel <- server
+		return
+	}
+
+	if reply.voteGranted {
+		voteChannel <- 1
+	} else if reply.Term > args.CandidateTerm {
+		//  Another server is leader: return to follower state
+		rf.mu.Lock()
+		rf.lastHeardFromLeader = time.Now()
+		rf.votedFor = -1
+		rf.state = Follower
+		rf.currentTerm = reply.Term
+		rf.mu.Unlock()
+	}
+	voteChannel <- 0
 }
 
 // startElection method
@@ -71,38 +95,33 @@ func (rf *Raft) startElection() {
 	peerCount := len(rf.peers)
 	instanceId := rf.me
 	votesNeeded := (peerCount / 2) + 1
+	voteCount := 0
+	rf.lastHeardFromLeader = time.Now()
 
-	es := ElectionState{voteCount: 1} // votes for self
-	args := RequestVoteArgs{
-		CandidateId:           instanceId,
-		CandidateTerm:         rf.currentTerm,
-		CandidateLastLogIndex: len(rf.logs) - 1,
-		CandidateLastLogTerm:  rf.logs[len(rf.logs)-1].Term,
-	}
-
+	voteChannel := make(chan int, peerCount-1)
+	errChannel := make(chan int, peerCount-1)
 	rf.mu.Unlock()
 
 	// issues `RequestVote RPCs` in parallel
 	for server := range peerCount {
 		if server != instanceId {
-			go rf.sendRequestVote(server, &es, &args)
+			go rf.sendRequestVote(server, voteChannel, errChannel)
 		}
 	}
 
-	// Outcome 2: a follower is actually a leader
-	if es.convertToFollower {
-		rf.mu.Lock()
-		rf.currentTerm = es.newTerm
-		rf.state = Follower
-		rf.lastHeardFromLeader = time.Now()
-		rf.votedFor = -1
-		rf.mu.Unlock()
-	} else if es.voteCount >= votesNeeded {
-		// Outcome 1: elected to leader
+	for range peerCount - 1 {
+		select {
+		case vote := <-voteChannel:
+			voteCount += vote
+		}
+	}
+
+	// Outcome 1: elected to leader
+	if voteCount >= votesNeeded {
 		rf.mu.Lock()
 		rf.state = Leader
 		rf.mu.Unlock()
-		// TODO: send heartbeats
+		rf.sendHeartbeats()
 	}
-	// Outcome 3: repeate election
+	// Outcome 3: repeat election
 }
