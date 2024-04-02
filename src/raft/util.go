f1bcf538947a7335be28a1cd90de9c2507154bf0
dave-hay <davehay93@gmail.com>
2024-04-02 14:39:00 -0700

    refactor

--- src/raft/util.go
+++ src/raft/util.go
@@ -25,10 +25,9 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-var debug bool = false
-
-func Debugf(format string, v ...interface{}) {
-	if debug {
-		log.Printf(format, v...)
+func DPrintln(text string) (n int, err error) {
+	if Debug > 0 {
+		log.Println(text)
 	}
+	return
 }
