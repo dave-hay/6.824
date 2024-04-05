08487ff101b0ed04f96f4a2f2ad3fc86aba358cd
dave-hay <davehay93@gmail.com>
2024-04-05 12:07:26 -0700

    etc(raft.go): ignore lint unused funcs

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -215,6 +215,8 @@ func (rf *Raft) Start(command interface{}) (int, int, bool) {
 // save Raft's persistent state to stable storage,
 // where it can later be retrieved after a crash and restart.
 // see paper's Figure 2 for a description of what should be persistent.
+//
+//lint:ignore U1000 Ignore unused function temporarily for debugging
 func (rf *Raft) persist() {
 	// Your code here (2C).
 	// Example:
@@ -227,6 +229,8 @@ func (rf *Raft) persist() {
 }
 
 // restore previously persisted state.
+//
+//lint:ignore U1000 Ignore unused function temporarily for debugging
 func (rf *Raft) readPersist(data []byte) {
 	if data == nil || len(data) < 1 { // bootstrap without any state?
 		return
