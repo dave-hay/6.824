72b915b479728b130bb9c30e54a5cf180618fc51
dave-hay <davehay93@gmail.com>
2024-04-06 15:11:12 -0700

    moved Start() -> state.go

--- src/raft/state.go
+++ src/raft/state.go
@@ -26,3 +26,35 @@ func (rf *Raft) apply(nextIndex int, leaderLogs []LogEntry) {
 
 	rf.applyCh <- msg
 }
+
+// Start()
+// the service using Raft (e.g. a k/v server) wants to start
+// agreement on the next command to be appended to Raft's log.
+// start the agreement and return immediately.
+// no guarantee command will be committed to the Raft log
+//
+// index (int): index the command will appear if commited. len(rf.logs) + 1
+// term (int): current term
+// isLeader (bool): if server is leader
+func (rf *Raft) Start(command interface{}) (int, int, bool) {
+	DPrint(rf.me, "Start()", "command: %v", command)
+	index := -1
+	term := -1
+	isLeader := false
+
+	if rf.getState() != Leader {
+		return index, term, isLeader
+	}
+
+	rf.mu.Lock()
+	term = rf.currentTerm
+	rf.logs = append(rf.logs, LogEntry{Term: term, Command: command}) // append command to log
+	index = len(rf.logs) - 1
+	isLeader = rf.state == Leader
+	rf.mu.Unlock()
+
+	// fire off AppendEntries
+	go rf.sendLogEntries()
+
+	return index, term, isLeader
+}
