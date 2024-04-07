c6dbcb8782293c74183156a3d7e269a872413b37
dave-hay <davehay93@gmail.com>
2024-04-07 16:13:59 -0700

    encoding: individual funcs

--- src/raft/encoding.go
+++ src/raft/encoding.go
@@ -3,10 +3,54 @@ package raft
 import (
 	"bytes"
 	"compress/gzip"
+	"encoding/gob"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 )
+
+func EncodeToBytes(p interface{}) []byte {
+	buf := bytes.Buffer{}
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("uncompressed size (bytes): ", len(buf.Bytes()))
+	return buf.Bytes()
+}
+
+func Compress(s []byte) []byte {
+	zipbuf := bytes.Buffer{}
+	zipped := gzip.NewWriter(&zipbuf)
+	zipped.Write(s)
+	zipped.Close()
+	fmt.Println("compressed size (bytes): ", len(zipbuf.Bytes()))
+	return zipbuf.Bytes()
+}
+
+func Decompress(s []byte) []byte {
+	rdr, _ := gzip.NewReader(bytes.NewReader(s))
+	data, err := io.ReadAll(rdr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rdr.Close()
+	fmt.Println("uncompressed size (bytes): ", len(data))
+	return data
+}
+
+func DecodeToLogs(s []byte) []LogEntry {
+
+	p := []LogEntry{}
+	dec := gob.NewDecoder(bytes.NewReader(s))
+	err := dec.Decode(&p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return p
+}
 
 func encodeLogs(logs []LogEntry) []byte {
 	encodedLogs, err := encodePayload(logs)
