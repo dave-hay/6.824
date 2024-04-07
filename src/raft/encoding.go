fe51c8f30f772d19754b4bd5e890ffacdf57b753
dave-hay <davehay93@gmail.com>
2024-04-07 16:27:31 -0700

    encoding: rm unused funcs

--- src/raft/encoding.go
+++ src/raft/encoding.go
@@ -4,8 +4,6 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/gob"
-	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 )
@@ -17,7 +15,6 @@ func EncodeToBytes(p interface{}) []byte {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("uncompressed size (bytes): ", len(buf.Bytes()))
 	return buf.Bytes()
 }
 
@@ -26,7 +23,6 @@ func Compress(s []byte) []byte {
 	zipped := gzip.NewWriter(&zipbuf)
 	zipped.Write(s)
 	zipped.Close()
-	fmt.Println("compressed size (bytes): ", len(zipbuf.Bytes()))
 	return zipbuf.Bytes()
 }
 
@@ -37,7 +33,6 @@ func Decompress(s []byte) []byte {
 		log.Fatal(err)
 	}
 	rdr.Close()
-	fmt.Println("uncompressed size (bytes): ", len(data))
 	return data
 }
 
@@ -50,114 +45,4 @@ func DecodeToLogs(s []byte) []LogEntry {
 		log.Fatal(err)
 	}
 	return p
-}
-
-func encodeLogs(logs []LogEntry) []byte {
-	encodedLogs, err := encodePayload(logs)
-
-	if err != nil {
-		log.Printf("Failed to encode arguments: %v", err)
-	}
-
-	return encodedLogs
-}
-
-func decodeLogs(encodedLogs []byte) []LogEntry {
-	decodedLogs := []LogEntry{}
-
-	// Decode the reply
-	err := decodePayload(encodedLogs, &decodedLogs)
-	if err != nil {
-		log.Printf("Failed to decode reply: %v", err)
-	}
-
-	return decodedLogs
-}
-
-func encodePayload(data interface{}) ([]byte, error) {
-	// Marshal the data into JSON format
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		return nil, err
-	}
-
-	// Create a new gzip writer
-	var buf bytes.Buffer
-	gzipWriter := gzip.NewWriter(&buf)
-
-	// Write the JSON data to the gzip writer
-	_, err = gzipWriter.Write(jsonData)
-	if err != nil {
-		return nil, err
-	}
-
-	// Close the gzip writer to flush any remaining data
-	err = gzipWriter.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	// Get the compressed payload
-	compressedPayload := buf.Bytes()
-
-	return compressedPayload, nil
-}
-
-func decodePayload(compressedPayload []byte, data interface{}) error {
-	// Create a new gzip reader
-	gzipReader, err := gzip.NewReader(bytes.NewReader(compressedPayload))
-	if err != nil {
-		return err
-	}
-	defer gzipReader.Close()
-
-	// Read the compressed data
-	jsonData, err := io.ReadAll(gzipReader)
-	if err != nil {
-		return err
-	}
-
-	// Unmarshal the JSON data into the provided data structure
-	err = json.Unmarshal(jsonData, data)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func decodeParams(encodedArgs []byte, encodedReply []byte) (*AppendEntriesArgs, *AppendEntriesReply) {
-
-	decodedReply := &AppendEntriesReply{}
-	decodedArgs := &AppendEntriesArgs{}
-
-	// Decode the reply
-	err := decodePayload(encodedReply, decodedReply)
-	if err != nil {
-		log.Printf("Failed to decode reply: %v", err)
-	}
-
-	err = decodePayload(encodedArgs, decodedArgs)
-
-	if err != nil {
-		log.Printf("Failed to decode reply: %v", err)
-	}
-
-	return decodedArgs, decodedReply
-}
-
-func encodeParams(args interface{}, reply interface{}) ([]byte, []byte) {
-	encodedArgs, err := encodePayload(args)
-
-	if err != nil {
-		log.Printf("Failed to encode arguments: %v", err)
-	}
-
-	encodedReply, err := encodePayload(reply)
-
-	if err != nil {
-		log.Printf("Failed to encode arguments: %v", err)
-	}
-
-	return encodedArgs, encodedReply
 }
