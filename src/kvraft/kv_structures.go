1ce3c0cb18d3a79a4bf2d32dc81401c4cb176e00
dave-hay <davehay93@gmail.com>
2024-05-06 14:50:38 -0700

    ds: rm q add channel map

--- src/kvraft/kv_structures.go
+++ src/kvraft/kv_structures.go
@@ -2,37 +2,6 @@ package kvraft
 
 import "sync"
 
-type KVQueue struct {
-	mu    sync.Mutex
-	items []Op
-}
-
-func makeKVQueue() *KVQueue {
-	return &KVQueue{}
-}
-
-func (kvq *KVQueue) length() int {
-	kvq.mu.Lock()
-	defer kvq.mu.Unlock()
-	return len(kvq.items)
-}
-
-func (kvq *KVQueue) append(item Op) {
-	kvq.mu.Lock()
-	defer kvq.mu.Unlock()
-	kvq.items = append(kvq.items, item)
-}
-
-func (kvq *KVQueue) pop() {
-	kvq.mu.Lock()
-	defer kvq.mu.Unlock()
-	if kvq.length() == 0 {
-		return
-	}
-
-	kvq.items = kvq.items[1:]
-}
-
 type KVMap struct {
 	mu    sync.Mutex
 	items map[string]string
@@ -58,4 +27,43 @@ func (kvm *KVMap) append(key string, val string) {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
 	kvm.items[key] += val
+}
+
+func (kvm *KVMap) del(key string) {
+	kvm.mu.Lock()
+	defer kvm.mu.Unlock()
+	delete(kvm.items, key)
+}
+
+type Resp struct {
+	Data chan string
+	Err  chan Err
+}
+
+type KVChanMap struct {
+	mu    sync.Mutex
+	items map[int64]chan bool
+}
+
+func makeKVChanMap() *KVChanMap {
+	return &KVChanMap{}
+}
+
+func (kvm *KVChanMap) get(key int64) chan bool {
+	kvm.mu.Lock()
+	defer kvm.mu.Unlock()
+	return kvm.items[key]
+}
+
+func (kvm *KVChanMap) add(key int64) chan bool {
+	kvm.mu.Lock()
+	defer kvm.mu.Unlock()
+	kvm.items[key] = make(chan bool, 1)
+	return kvm.items[key]
+}
+
+func (kvm *KVChanMap) del(key int64) {
+	kvm.mu.Lock()
+	defer kvm.mu.Unlock()
+	delete(kvm.items, key)
 }
