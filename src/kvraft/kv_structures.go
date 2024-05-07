b814a3dce35f58aa187b912a00a101fca8b595b7
dave-hay <davehay93@gmail.com>
2024-05-06 20:33:04 -0700

    debug messages

--- src/kvraft/kv_structures.go
+++ src/kvraft/kv_structures.go
@@ -14,18 +14,21 @@ func makeKVMap() *KVMap {
 func (kvm *KVMap) get(key string) string {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
+	DPrintf("DB get key: %s", key)
 	return kvm.items[key]
 }
 
 func (kvm *KVMap) put(key string, val string) {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
+	DPrintf("DB put key: %s, val: %s", key, val)
 	kvm.items[key] = val
 }
 
 func (kvm *KVMap) append(key string, val string) {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
+	DPrintf("DB put key: %s, val: %s", key, val)
 	kvm.items[key] += val
 }
 
@@ -65,6 +68,7 @@ func makeKVChanMap() *KVChanMap {
 func (kvm *KVChanMap) get(key int64) ChanMapEntry {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
+	DPrintf("get items[key](%d): %v", key, kvm.items[key])
 	return kvm.items[key]
 }
 
@@ -72,12 +76,14 @@ func (kvm *KVChanMap) contains(key int64) bool {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
 	_, ok := kvm.items[key]
+	DPrintf("contains key: %d: %t", key, ok)
 	return ok
 }
 
 func (kvm *KVChanMap) add(key int64) ChanMapEntry {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
+	DPrintf("adding key: %d", key)
 	kvm.items[key] = ChanMapEntry{ch: make(chan bool, 1), status: NEW}
 	return kvm.items[key]
 }
@@ -85,13 +91,14 @@ func (kvm *KVChanMap) add(key int64) ChanMapEntry {
 func (kvm *KVChanMap) del(key int64) {
 	kvm.mu.Lock()
 	defer kvm.mu.Unlock()
+	DPrintf("deleting key: %d", key)
 	delete(kvm.items, key)
 }
 
-func (kvm *KVChanMap) setStatus(key int64, status ItemStatus) {
-	kvm.mu.Lock()
-	defer kvm.mu.Unlock()
-	item := kvm.items[key]
-	item.status = status
-	kvm.items[key] = item
-}
+// func (kvm *KVChanMap) setStatus(key int64, status ItemStatus) {
+// 	kvm.mu.Lock()
+// 	defer kvm.mu.Unlock()
+// 	item := kvm.items[key]
+// 	item.status = status
+// 	kvm.items[key] = item
+// }
