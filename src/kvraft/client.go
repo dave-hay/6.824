0a4ea8561f60a9b42c0f34268c2a8f32228bf32c
dave-hay <davehay93@gmail.com>
2024-05-06 18:37:39 -0700

    client.go: Get

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -3,6 +3,8 @@ package kvraft
 import (
 	"crypto/rand"
 	"math/big"
+	"sync"
+	"time"
 
 	"6.824/labrpc"
 )
@@ -10,6 +12,8 @@ import (
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	mu       sync.RWMutex
+	leaderId int
 }
 
 func nrand() int64 {
@@ -26,6 +30,18 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	return ck
 }
 
+func (ck *Clerk) getLeaderId() int {
+	ck.mu.RLock()
+	defer ck.mu.RUnlock()
+	return ck.leaderId
+}
+
+func (ck *Clerk) setLeaderId(id int) {
+	ck.mu.Lock()
+	defer ck.mu.Unlock()
+	ck.leaderId = id
+}
+
 // fetch the current value for a key.
 // returns "" if the key does not exist.
 // keeps trying forever in the face of all other errors.
@@ -37,9 +53,37 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 // must match the declared types of the RPC handler function's
 // arguments. and reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) string {
-
 	// You will have to modify this function.
-	return ""
+	if key == "" {
+		return ""
+	}
+	id := nrand()
+	args := &GetArgs{Id: id, Key: key}
+	reply := &GetReply{}
+
+	for {
+		leader := ck.getLeaderId()
+		ck.sendGet(leader, args, reply)
+		switch reply.Err {
+		case OK:
+			return reply.Value
+		case ErrNoKey:
+			args.Key = key
+		case ErrWrongLeader:
+			ck.setLeaderId(reply.LeaderId)
+		case ErrExecuted:
+			return ""
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
+
+}
+
+// want to know if success or if it needs to be called again
+func (ck *Clerk) sendGet(server int, args *GetArgs, reply *GetReply) {
+	if ok := ck.servers[server].Call("KVServer.Get", args, reply); !ok {
+		reply.Err = ErrNetwork
+	}
 }
 
 // shared by Put and Append.
